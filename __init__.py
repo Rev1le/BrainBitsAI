3fc308cb96b11fd9442b0548b1fb09292837f91a
Rev1le <59996773+Rev1le@users.noreply.github.com>
2022-06-23 23:16:30 +0500

    Update __init__.py
    Добавлена поддержка путей из конфига.

--- __init__.py
+++ __init__.py
@@ -2,17 +2,31 @@ from yolov5facedetector.face_detector import YoloDetector
 
 import numpy as np
 from PIL import Image
-from random import randint
 import cv2
+import uuid
+import os
+import shutil
+import json
 
-SAVING_FRAMES_PER_SECOND = 10
+
+with open("config.json", encoding='utf-8') as f:
+    const_data = json.load(f)
+
+PATH_PROJECT = os.getcwd()
+PATH_FOR_FACES = const_data['path_for_face']
+MIN_FACE_PERCENT = const_data['min_face_percent']
+PATH_TRAINING_VIDEO = const_data['path_url_traning_video']
+SAVING_FRAMES_PER_SECOND = const_data['fps_received']
 
 def analysis_image(img_array):
     bboxes, confs, points = model.predict(img_array)
     list_face_coords: list = bboxes[0]
     print(list_face_coords)
-    cv2.imwrite(f'D:\\Project\\Python\\AI\\frame.jpg', img_array)
-    image = Image.open(f'D:\\Project\\Python\\AI\\frame.jpg')
+
+    path_frame_tmp: str = f'{PATH_PROJECT}\\frame.jpg'
+    cv2.imwrite(path_frame_tmp, img_array)
+    image = Image.open(path_frame_tmp)
+
     save_faces(list_face_coords, image)
     return True
 
@@ -20,32 +34,34 @@ def save_faces(list_face_coords, image):
     for face_coords in list_face_coords:
         cropped_img = image.crop((face_coords))
         cropped_img = cropped_img.resize((512, 512))
-        cropped_img.save(f"D:\\Project\\Python\\AI\\лица\\лицо{randint(0, 99999)}.jpg",  quality=95)
+        cropped_img.save(f"{PATH_FOR_FACES}\\лицо_{uuid.uuid4()}.jpg",  quality=95)
     return True
 
+def clean_folder_faces():
+    file_path_folder = PATH_FOR_FACES
+    shutil.rmtree(file_path_folder)
+    os.mkdir(file_path_folder)
 
 
-model = YoloDetector(gpu=0, min_face=50) # Will download weight file automatically
+model = YoloDetector(gpu=0, min_face=70) # Will download weight file automatically
 
-
-video_path = "D:\\Project\\Python\\AI\\traning_video.mp4"
-rtspVideo = cv2.VideoCapture(video_path)
+rtspVideo = cv2.VideoCapture(PATH_TRAINING_VIDEO)
 
 print(rtspVideo.get(cv2.CAP_PROP_FRAME_COUNT))
 print(rtspVideo.get(cv2.CAP_PROP_FPS))
 
 count: int = 0
 
+clean_folder_faces()
+
 while rtspVideo.isOpened():
     ret, frame_nparray = rtspVideo.read()
 
-    if ret:
-        count += 1
+    if ret: count += 1
     else:
-        print("frame read failed")
+        print("Frame read failed")
         break
 
     if count == 30:
-
         analysis_image(frame_nparray)
         count = 0
